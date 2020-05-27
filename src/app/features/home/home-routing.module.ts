242aa331b662d938608bc72c6851f9f5d7a81f79
luca.alicata <luca.alicata@lynxspa.com>
2020-05-27 10:12:34 +0200

    lazy loading feature module con router outlet

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/main/home.component';
+import { CoroselloComponent } from './components/corosello/corosello.component';
 
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{
+  path: '', component: HomeComponent, children: [
+    {path: "carosello", component: CoroselloComponent}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
