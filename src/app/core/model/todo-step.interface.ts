c0db7e551804d29b562ef752e3a557e9e36cf674
luca.alicata <luca.alicata@lynxspa.com>
2020-06-16 12:09:29 +0200

    commenti

--- src/app/core/model/todo-step.interface.ts
+++ src/app/core/model/todo-step.interface.ts
@@ -1,5 +1,17 @@
-export interface TodoStep{
+/**
+ * interfaccia che rappresenta lo step di un Todo
+ */
+export interface TodoStep {
+    /**
+     * Definisce l'id dello step
+     */
     id: number;
+    /**
+     * il titolo dello step
+     */
     title: string;
+    /**
+     * indica se lo step è stato eseguito
+     */
     done: boolean;
 }
