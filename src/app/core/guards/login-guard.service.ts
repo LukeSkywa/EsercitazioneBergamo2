7790fe3df9f3c167b43a5f6676697ab7dfe541fe
luca.alicata <luca.alicata@lynxspa.com>
2020-07-08 10:23:40 +0200

    mantenimento current user al riavvio

--- src/app/core/guards/login-guard.service.ts
+++ src/app/core/guards/login-guard.service.ts
@@ -1,14 +1,18 @@
+import { Store } from '@ngrx/store';
+import { AuthService } from './../services/auth.service';
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
+import { saveCurrentUser } from 'src/app/redux/users/users.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuardService implements CanLoad {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private store: Store) { }
   canLoad(route: import("@angular/router").Route, segments: import("@angular/router").UrlSegment[]): boolean | import("rxjs").Observable<boolean> | Promise<boolean> {
     if (sessionStorage.getItem("user") != null) {
+      this.store.dispatch(saveCurrentUser({user: JSON.parse(sessionStorage.getItem("user"))}));
       return true;
     } else {
       this.router.navigateByUrl("/login");
--- src/app/features/login/services/login.service.ts
+++ src/app/features/login/services/login.service.ts
@@ -2,7 +2,6 @@ import { Store } from '@ngrx/store';
 import { AuthService } from './../../../core/services/auth.service';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoginModule } from '../login.module';
 import { User } from 'src/app/core/model/user.interface';
 import { saveCurrentUser } from 'src/app/redux/users/users.actions';
 
@@ -14,7 +13,7 @@ export class LoginService {
   executeLogin(username: string) {
     this.authService.doLogin(username).subscribe((users: User[]) => {
       if (users && users.length > 0) {
-        sessionStorage.setItem("user", username);
+        sessionStorage.setItem("user", JSON.stringify(users[0]));
         this.store.dispatch(saveCurrentUser({user: users[0]}));
         this.router.navigateByUrl("/home");
       }else{
