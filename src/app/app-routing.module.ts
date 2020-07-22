cfe1b3a7194a93aeb8d7c493e3442573e2b2bfa8
luca.alicata <luca.alicata@lynxspa.com>
2020-07-22 11:45:27 +0200

    scrittura dei primi test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginGuardService } from './core/guards/login-guard.service';
+import { LoggedGuardService } from './core/guards/logged-guard.service';
 
 
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
+  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule), canLoad: [LoggedGuardService] },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule), canLoad: [LoginGuardService] },
   { path: 'todos', loadChildren: () => import('./features/todos/todos.module').then(m => m.TodosModule), canLoad: [LoginGuardService] },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
